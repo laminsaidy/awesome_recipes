c34646b8a8525921171d190dc4fee8c73db4fcae
LX <132520325+laminsaidy@users.noreply.github.com>
2024-08-22 20:49:03 +0000

    Ready codes for deployment

--- main/settings.py
+++ main/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-_x7fup+=x5z%4ob5dw0av99wo7(54ft2mftzrkp15^c=d=!=13"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com',]
 
 
 # Application definition
